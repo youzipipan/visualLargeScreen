4f7889f5977ab640016a9c81ce688758b4902ab3
HS\71644463 <xingzhen.liu.er@hitachi.cn>
2023-09-04 21:06:21 +0800

    提交

--- src/main/java/com/pg/screen/dao/DpGdkkxZbDao.java
+++ src/main/java/com/pg/screen/dao/DpGdkkxZbDao.java
@@ -220,7 +220,7 @@ public class DpGdkkxZbDao {
             sql = "select * " +
                     "from (select SSXQ, " + columns + " " +
                     "      from DP_GDKKX_ZB " +
-                    "      where SSXQ is not null  " +
+                    "      where (SSXQ is not null and SSXQ!='大连')  " +
                     "        and DQTZ = '市中心+市区+城镇+农村' " +
                     "        and QSSJ >= TO_DATE('" + beginDate + "', 'yyyy-mm-dd') " +
                     "        and ZZSJ <= TO_DATE('" + endDate + "', 'yyyy-mm-dd') " +
@@ -229,7 +229,7 @@ public class DpGdkkxZbDao {
         } else {
             sql = "select SSXQ, " + columns + " " +
                     "from DP_GDKKX_ZB " +
-                    "where SSXQ is not null  " +
+                    "where SSXQ is not null and SSXQ!='大连'  " +
                     "  and DQTZ = '市中心+市区+城镇+农村' " +
                     "  and QSSJ >= TO_DATE('" + beginDate + "', 'yyyy-mm-dd') " +
                     "  and ZZSJ <= TO_DATE('" + endDate + "', 'yyyy-mm-dd')";
